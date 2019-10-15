1a9b7dc6bc98a481c5066ef2f3cd9d1685323244
Alex Marchant <alexjmarchant@gmail.com>
2019-10-15 13:28:25 -0400

    Simplify interfaces

--- slides/2-interfaces.ts
+++ slides/2-interfaces.ts
@@ -1,16 +1,29 @@
 // How do you specify complex objects?
 interface Test {
   id: number
-  payload: {
-    status: string
-  }
+  status: string
 }
 
 const test: Test = {
   id: 1,
-  payload: {
-    status: 'Draft'
-  }
+  status: 'Draft'
 }
 console.log(test.id) // Valid prop
-console.log(test.payload.testType) // Invalid prop
+console.log(test.testType) // Invalid prop
+
+
+
+
+// Extensible
+interface TestWithType extends Test {
+  testType: string
+}
+
+const testWithType: TestWithType = {
+  id: 1,
+  status: 'Draft',
+  testType: 'User Discovery'
+}
+
+console.log(testWithType.id) // Valid prop
+console.log(testWithType.testType) // Now valid prop
